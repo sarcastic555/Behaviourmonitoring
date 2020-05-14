1886eecd515e48a7574abced4684ff0d8b662258
sarcastic555 <Unkown>
2020-05-14 23:49:46 +0900

    automatic filename definition in downloading script

--- download_files.sh
+++ download_files.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
 
-adb shell "run-as com.example.behaviourmonitoring cp /data/user/0/com.example.behaviourmonitoring/files/20200411.csv /sdcard/"
-adb pull /sdcard/20200411.csv ~/Desktop/android_data
+start_time=`date +%s -d '2020/05/10'`
+back=0
+date "+%s" -d "-${back} days"
+echo ${start_time}
+while [ `date +%s -d "-${back} days"` -gt ${start_time} ]; do
+    datestring=`date "+%Y%m%d" -d "-${back} days"`
+    echo ${datestring}
+    adb shell "run-as com.example.behaviourmonitoring cp /data/user/0/com.example.behaviourmonitoring/files/${datestring}.csv /sdcard/"
+    adb pull /sdcard/${datestring}.csv ~/Desktop/android_data/
+    back=`expr ${back} + 1`
+done
